6d23ba598e5c13121479f790b74cecb76fc7f276
Deft Valian Exanova <deftvalian2411@gmail.com>
2024-09-24 17:26:00 +0700

    delete dewakd.jsx and deft.jsx

--- src/deft.jsx
+++
@@ -1,5 +0,0 @@
-function deft() {
-    return <h1>Ini dep</h1>
-}
-
-export default deft;
--- src/dewakd.jsx
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-
-export default function dewakd() {
-  return (
-    <div className='text-3xl font-bold underline'>dewakd</div>
-  )
-}
